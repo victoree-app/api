5e50dd32785e8e301b90b4f472d646da498f3819
Shyam <24shyam7@gmail.com>
2020-02-10 15:00:35 -0500

    get project owner details

--- src/main/java/com/victoree/api/controllers/AuthenticationController.java
+++ src/main/java/com/victoree/api/controllers/AuthenticationController.java
@@ -2,15 +2,20 @@ package com.victoree.api.controllers;
 
 import com.victoree.api.domains.AuthenticationResponse;
 import com.victoree.api.domains.LoginRequest;
+import com.victoree.api.domains.UserResponse;
 import com.victoree.api.exceptions.UnauthorizedLoginException;
+import com.victoree.api.exceptions.UnauthorizedRequestException;
 import com.victoree.api.services.AuthenticationService;
 import com.victoree.api.validator.ValidatorUtil;
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -35,6 +40,23 @@ public class AuthenticationController {
       return new ResponseEntity(HttpStatus.UNAUTHORIZED);
     }
     return ResponseEntity.ok(authenticationResponse);
+  }
+
+  @GetMapping("/user")
+  public ResponseEntity getUser(@RequestHeader Map<String, String> headers) {
+    String sessionId = headers.getOrDefault("sessionid", null);
+    if (sessionId == null) {
+      return new ResponseEntity(HttpStatus.UNAUTHORIZED);
+    }
+    try {
+      String username = authenticationService.getUserNameFromSessionId(sessionId);
+      if (username == null) {
+        return new ResponseEntity(HttpStatus.UNAUTHORIZED);
+      }
+      return ResponseEntity.ok(new UserResponse(username));
+    } catch (UnauthorizedRequestException e) {
+      return new ResponseEntity(HttpStatus.UNAUTHORIZED);
+    }
   }
 
 }
---
+++ src/main/java/com/victoree/api/domains/UserResponse.java
@@ -0,0 +1,14 @@
+package com.victoree.api.domains;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserResponse {
+  private String username;
+}
